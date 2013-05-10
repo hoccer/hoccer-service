2892f10a2f9c2cbee5df84bcb231198cbc2155e1
Ingo Albrecht <prom@berlin.ccc.de>
2013-05-10 11:44:27 +0200

    First draft of control protocol

--- filecache/src/main/java/com/hoccer/filecache/control/ControlConnection.java
+++ filecache/src/main/java/com/hoccer/filecache/control/ControlConnection.java
@@ -1,7 +1,10 @@
 package com.hoccer.filecache.control;
 
 import better.jsonrpc.core.JsonRpcConnection;
+import com.hoccer.filecache.CacheBackend;
 import com.hoccer.filecache.control.ICacheControl;
+
+import javax.servlet.ServletContext;
 
 public class ControlConnection implements ICacheControl {
 
@@ -9,9 +12,36 @@ public class ControlConnection implements ICacheControl {
 
     JsonRpcConnection mRpcConnection;
 
+    CacheBackend mBackend;
+
     public ControlConnection(ControlServlet servlet, JsonRpcConnection rpcConnection) {
         mServlet = servlet;
         mRpcConnection = rpcConnection;
+        mBackend = getCacheBackend();
+    }
+
+    private CacheBackend getCacheBackend() {
+        ServletContext ctx = mServlet.getServletContext();
+        CacheBackend backend = (CacheBackend)ctx.getAttribute("backend");
+        return backend;
+    }
+
+    @Override
+    public FileHandles createFileForStorage(String accountId, int fileSize) {
+        return null;
+    }
+
+    @Override
+    public FileHandles createFileForTransfer(String accountId, int fileSize) {
+        return null;
+    }
+
+    @Override
+    public void deleteFile(String fileId) {
+    }
+
+    @Override
+    public void deleteAccount(String accountId) {
     }
 
 }
