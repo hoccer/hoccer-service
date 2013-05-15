e0d8e0072b68e92d1e0881a64cd9cbe08c603dd7
Ingo Albrecht <prom@berlin.ccc.de>
2013-05-15 22:40:05 +0200

    Implemented part of the control protocol

--- filecache/src/main/java/com/hoccer/filecache/control/ControlConnection.java
+++ filecache/src/main/java/com/hoccer/filecache/control/ControlConnection.java
@@ -3,8 +3,10 @@ package com.hoccer.filecache.control;
 import better.jsonrpc.core.JsonRpcConnection;
 import com.hoccer.filecache.CacheBackend;
 import com.hoccer.filecache.control.ICacheControl;
+import com.hoccer.filecache.model.CacheFile;
 
 import javax.servlet.ServletContext;
+import java.util.UUID;
 
 public class ControlConnection implements ICacheControl {
 
@@ -28,16 +30,29 @@ public class ControlConnection implements ICacheControl {
 
     @Override
     public FileHandles createFileForStorage(String accountId, int fileSize) {
-        return null;
+        CacheFile file = mBackend.forId(UUID.randomUUID().toString(), true);
+        FileHandles handles = new FileHandles();
+        handles.fileId = file.getFileId();
+        handles.uploadId = file.getUploadId();
+        handles.downloadId = file.getDownloadId();
+        mBackend.checkpoint(file);
+        return handles;
     }
 
     @Override
     public FileHandles createFileForTransfer(String accountId, int fileSize) {
-        return null;
+        CacheFile file = mBackend.forId(UUID.randomUUID().toString(), true);
+        FileHandles handles = new FileHandles();
+        handles.fileId = file.getFileId();
+        handles.uploadId = file.getUploadId();
+        handles.downloadId = file.getDownloadId();
+        mBackend.checkpoint(file);
+        return handles;
     }
 
     @Override
     public void deleteFile(String fileId) {
+        CacheFile file = mBackend.forId(fileId, false);
     }
 
     @Override
